3462d6522d48f53afc4bcdd3a346f42e4259ef12
Kirnkk <123626342+Kirnkk@users.noreply.github.com>
2023-07-11 17:26:29 +0530

    Add files via upload

--- UnlockKey.jsx
+++ UnlockKey.jsx
@@ -1,47 +1,94 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useState } from 'react'
 
 const UnlockKey = () => {
 
-  const initialState ={
-    input :'' ,
 
-  }
+  const [pass,setPass]=useState(0)
+
   const reducer=(state,action)=>{
 
     switch(action.type){
-      case 'PRESS_BUTTON':
-
-       const {input} = state;
-      const newInput = input +action.payload;
-           if(newInput === '5152'){
-        alert('lock unlocked')
-           }
-        return {input : newInput };
-
-        case'reset':
-        return initialState;
-
-      default : return state;
+      case 'PRESS_BUTTON':{
+        switch(action.payload){
+          case "1":{
+            if(pass===5){
+              return state='Access Granted! Safe UnLocked.'
+            }
+            else{
+              setPass(0);
+              return state="Reset!"
+            }
+          }
+          case "5":{
+            if(pass===2){
+              setPass(pass+1)
+              return state='***'
+            }
+            else{
+              setPass(0);
+              return state="Reset!"
+            }
+          }
+          case "9":{
+            if(pass===3){
+              setPass(pass+1)
+              return state='****'
+            }
+            else{
+              setPass(0);
+              return state="Reset!"
+            }
+          }
+          case "A":{
+            if(pass===0){
+              setPass(pass+1)
+              return state='*'
+            }
+            else{
+              setPass(0);
+              return state="Reset!"
+            }
+          }
+          case "D":{
+            if(pass===4){
+              setPass(pass+1)
+              return state='*****'
+            }
+            else{
+              setPass(0);
+              return state="Reset!"
+            }
+          }
+          case "S":{
+            if(pass===1){
+              setPass(pass+1)
+              return state='**'
+            }
+            else{
+              setPass(0);
+              return state="Reset!"
+            }
+          }
+          default : return 'Try Again'
+        }
+      }
+    default :return state='Try Again With Right Password!';
     }
 
   }
-
-  const[state,dispatch] = useReducer(reducer,initialState)
-
-  const handleButton =(value) =>{
-    dispatch({type: 'PRESS_BUTTON' ,payload:value})
-  }
+  const[state,dispatch] = useReducer(reducer,'Enter Password')
   return (
     <div>
-
-      <p>input:{state.input}</p>
-      <button onClick={()=>handleButton('1')}>1</button>&nbsp;&nbsp;&nbsp;
-      <button onClick={()=>handleButton('2')}>2</button>&nbsp;&nbsp;&nbsp;
-      <button onClick={()=>handleButton('3')}>3</button><br/><br/>
-      <button onClick={()=>handleButton('4')}>4</button>&nbsp;&nbsp;&nbsp;
-      <button onClick={()=>handleButton('5')}>5</button>&nbsp;&nbsp;&nbsp;
-      <button onClick={()=>handleButton('6')}>6</button><br/><br/>
-      <button onClick ={()=>dispatch({type:'reset'})}>Reset</button>
+       <h2>KeyPad</h2>
+       <br/>
+       <hr/>
+      <p>Input:{state}</p>
+      <button onClick={()=>{dispatch({type:'PRESS_BUTTON',payload:'1'})}}>1</button>&nbsp;&nbsp;&nbsp;
+      <button onClick={()=>{dispatch({type:'PRESS_BUTTON',payload:'5'})}}>5</button>&nbsp;&nbsp;&nbsp;
+      <button onClick={()=>{dispatch({type:'PRESS_BUTTON',payload:'9'})}}>9</button><br/><br/>
+      <button onClick={()=>{dispatch({type:'PRESS_BUTTON',payload:'A'})}}>A</button>&nbsp;&nbsp;&nbsp;
+      <button onClick={()=>{dispatch({type:'PRESS_BUTTON',payload:'S'})}}>S</button>&nbsp;&nbsp;&nbsp;
+      <button onClick={()=>{dispatch({type:'PRESS_BUTTON',payload:'D'})}}>D</button><br/><br/>
     </div>
 )
 }
